af759aa90d09cbcdc6e7256a2eda558612392077
Prabanjan06 <117291126+Prabanjan06@users.noreply.github.com>
2024-03-23 14:21:42 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -24,47 +24,4 @@ st.header("Super Market Sales Report")
 st.write("DataFrame")
 st.write(df.describe())
 
-# Function to plot gender pie chart
-def plot_gender_pie_chart(data):
-    # Count the number of men and women
-    gender_counts = data['Gender'].value_counts()
 
-    # Plotting the pie chart
-    st.write('### Gender Distribution')
-    fig, ax = plt.subplots()
-    ax.pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', startangle=140)
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    st.pyplot(fig)
-
-def main():
-    st.title('Gender Distribution Analysis')
-    plot_gender_pie_chart(df)
-
-if __name__ == "__main__":
-    main()
-
-# Display Images
-
-# Checkbox
-# Check if the checkbox is checked
-# Title of the checkbox is 'Show/Hide'
-if st.checkbox("Show/Hide"):
-    # Display the text if the checkbox returns True value
-    st.text("Showing the widget")
-
-# Radio button
-# First argument is the title of the radio button
-# Second argument is the options for the radio button
-status = st.radio("Select Gender: ", ('Male', 'Female'))
-
-# Conditional statement to print
-# Male if male is selected else print female
-# Show the result using the success function
-if status == 'Male':
-    st.success("Male")
-else:
-    st.success("Female")
-
-st.sidebar.title("This text written in the sidebar");
-st.sidebar.button("click me")
-st.sidebar.radio("Pick ur gender",["male","female"])
