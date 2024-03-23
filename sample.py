03f1dc2ea32164753b1d63f6014d413c20af1d24
Prabanjan06 <117291126+Prabanjan06@users.noreply.github.com>
2024-03-23 12:07:54 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,11 @@
 # %%writefile sample.py
 import streamlit as st
 
-st.title('Welcome')
+# Title
+st.title("Guide for Streamlit !!!")
+# Header
+st.header("Certificate course on Business Analytics With Python")
+# Subheader
+st.subheader("offered by Dept of AMCS, TCE, Madurai")
+# Text
+st.text("Welcome you ALL!!!")
